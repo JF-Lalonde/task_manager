14ae9b00a4af6a21f552e9f8f38f401088560e24
JF-Lalonde <lalondej88@gmail.com>
2017-06-23 13:42:44 -0600

    added tasks table

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,7 +16,8 @@ class Task
   end
 
   def self.all
-    database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -24,14 +25,9 @@ class Task
   end
 
   def self.find(id)
-    database
-    task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
-    Task.new(task)
-  end
-
-  def self.database
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
-    database
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
+    Task.new(task)
   end
 end
